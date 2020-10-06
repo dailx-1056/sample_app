12d78ab554b853c94f7477802189c1a54a0ea78f
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2020-10-06 11:13:34 +0700

    Fix convention

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# you"ve limited to :test, :development, or :production.
+Bundler.require *Rails.groups
 
 module InitProject
   class Application < Rails::Application
